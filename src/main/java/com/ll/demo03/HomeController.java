c4d4a5d9ea7de1a65487152b9f5a6011c74089fc
danieljjs <danieljjs77@gmail.com>
2025-05-03 19:57:16 +0900

    work

--- src/main/java/com/ll/demo03/HomeController.java
+++ src/main/java/com/ll/demo03/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain(){
-        return "Hello World!, " + siteName;
+        return "Hello World!, on " + siteName + "!!!!!!!";
     }
 
     @GetMapping("/secretKey")
