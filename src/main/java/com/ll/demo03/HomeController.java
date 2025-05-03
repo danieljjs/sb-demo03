07c8924c9acf2a4407131f20a068bcca0b0d8b5b
danieljjs <danieljjs77@gmail.com>
2025-05-03 19:40:15 +0900

    work

--- src/main/java/com/ll/demo03/HomeController.java
+++ src/main/java/com/ll/demo03/HomeController.java
@@ -9,9 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @Value("${custom.site.name}")
     private String siteName;
+    @Value("${custom.secret.key}")
+    private String secretKey;
+
     @GetMapping("/")
     @ResponseBody
     public String showMain(){
         return "Hello World!, " + siteName;
+    }
+
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String showSecretKey(){
+        return "secretKey : " + secretKey;
     }
 }
