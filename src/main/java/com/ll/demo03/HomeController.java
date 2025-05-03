5a49407da99735516a4460c1e4a624aaefe12ada
danieljjs <danieljjs77@gmail.com>
2025-05-03 19:10:41 +0900

    "work"

---
+++ src/main/java/com/ll/demo03/HomeController.java
@@ -0,0 +1,17 @@
+package com.ll.demo03;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @Value("${custom.site.name}")
+    private String siteName;
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain(){
+        return "Hello World!, " + siteName;
+    }
+}
